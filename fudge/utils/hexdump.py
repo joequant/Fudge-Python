c5231fce6fa8ef7e7c2b1f5e7f05ab1d123a8fba
James Casey <jamesc.000@gmail.com>
2010-10-16 21:44:17 +0200

    Rewrote hexdump

--- fudge/utils/hexdump.py
+++ fudge/utils/hexdump.py
@@ -1,152 +1,74 @@
-#! /usr/bin/python
-"""hexdump.py - hex dump
-
-Ned Batchelder
-http://nedbatchelder.com
+#
+# Copyright CERN, 2010.
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+"""
+A Printer which outputs a hexdump version of a Fudge Message
 """
 
-# Broadly adapted from: http://www.kitebird.com/mysql-cookbook/
+import sys
 
-__version__ = "20080424"    # Change history at end of file.
 
-import getopt, sys
-
-def ascii(x):
-    """Determine how to show a byte in ascii."""
-    if 32 <= x <= 126:
-        return chr(x)
-    elif 160 <= x <= 255:
-        return '.'
+def ascii(val):
+    if 32 <= val <= 126:
+        return chr(val)
     else:
         return '.'
 
-def hexdump(f, width=16, verbose=0, start=0):
-    pos = 0
+ASC_MAP = [ ascii(x) for x in range(256) ]
 
-    ascmap = [ ascii(x) for x in range(256) ]
-    
-    lastbuf = ''
-    lastline = ''
-    nStarLen = 0
+class HexPrinter(object):
+    """Print a Fudge Message as a byte array.
 
-    if width > 4:
-        spaceCol = width//2
-    else:
-        spaceCol = -1
+    This basically isn't aware of any Fudge Structure, just
+    does a hexdump"""
 
-    hexwidth = 3 * width 
-    if spaceCol != -1:
-        hexwidth += 1                
+    def __init__(self, writer=sys.stdout, width=16):
+        """Create a new HexPrinter
 
-    if start:
-        f.seek(start)
-        pos = start
-        
-    while 1:
-        buf = f.read(width)
+        Arguments:
+            writer:  the writer stream to output to
+            width:  how many bytes per line (Default:16)
+        """
+        self._writer = writer
+        self._width = width
 
-        length = len(buf)
-        if length == 0:
-            if nStarLen:
-                if nStarLen > 1:
-                    print "* %d" % (nStarLen-1)
-                print lastline
-            return
+    def format(self, message):
+        """Output a formatted message to the underlying writer
 
-        bShowBuf = 1
-        
-        if not verbose and buf == lastbuf:
-            nStarLen += 1
-            bShowBuf = 0
-        else:
-            if nStarLen:
-                if nStarLen == 1:
-                    print lastline
-                else:
-                    print "* %d" % nStarLen
-            nStarLen = 0
+         Arguments:
+             message: the message to write
+        """
 
-        # Compose output line           
-        hex = ""
-        asc = ""
-        for i in range(length):
-            c = buf[i]
-            if i == spaceCol:
-                hex = hex + " "
-            hex = hex + ("%02x" % ord(c)) + " "
-            asc = asc + ascmap[ord(c)]
-        line = "%06x: %-*s %s" % (pos, hexwidth, hex, asc)
+        hexwidth = 3 * self._width + 1
+        start = 0
+        while start < len(message):
+            end = start + self._width
+            if end > len(message):
+                end = len(message)
 
-        if bShowBuf:
-            print line
-            
-        pos = pos + length
-        lastbuf = buf
-        lastline = line
-
-def main(args):
-
-    def usage():
-        for l in [
-            "hexdump: display data in hex",
-            "hexdump [opts] [file ...]",
-            "opts:",
-            " -s offset   start dumping from this offset",
-            " -v          show all data (else collapse duplicate lines)",
-            " -w width    show data this many bytes at a time (default 16)",
-            ]: print l
-        sys.exit()
-        
-    try:
-        opts, args = getopt.getopt(args, "vw:s:")
-    except getopt.GetoptError:
-        # print help information and exit:
-        usage()
-
-    options = {}
-    
-    for o, a in opts:
-        if o == '-s':
-            start = eval(a)
-            if type(start) != type(1) or start < 0:
-                usage()
-            options['start'] = start
-        elif o == '-v':
-            options['verbose'] = 1
-        elif o == '-w':
-            width = eval(a)
-            if type(width) != type(1) or not (1 <= width <= 100):
-                usage()
-            options['width'] = width
-        else:
-            usage()
-        
-    # Read stdin if no files were named, otherwise read each named file
-
-    if len(args) == 0:
-        hexdump(sys.stdin, **options)
-    else:
-        for name in args:
-            try:
-                f = open(name, "rb")
-            except IOError:
-                print >>sys.stderr, "Couldn't open %s" % name
-                continue
-            hexdump(f, **options)
-            f.close()
-
-if __name__ == '__main__':
-    try:
-        main(sys.argv[1:])
-    except KeyboardInterrupt:
-        print '\n-- interrupted --'
-    except IOError:
-        print '\n-- broken pipe --'
-        
-# Change history:
-# 20070722:
-#   Use integer division.
-#   Better error trapping, thanks Tim Hatch.
-# 20080424:
-#   Remove magic constant 49 for width of hex values as it only works correctly for default width 16.
-#   Fix duplicated last line when repetitions near end of file.
+            line = message[start:end]
+            hex_line = [ "%02x" % ord(val)+' ' for val in line]
+            # Add middle space
+            hex_line.insert(self._width/2, ' ')
+            asc_line = [ ASC_MAP[ord(val)] for val in line]
+            self._writer.write("%08x  %-*s |%s|\n" %
+                 (start, hexwidth, ''.join(hex_line), ''.join(asc_line)))
+            start += self._width
+        self._writer.write("%08x\n" % len(message))
